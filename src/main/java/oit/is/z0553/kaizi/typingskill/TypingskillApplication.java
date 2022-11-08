6e2539d104dbc15a9f789e525142257f25598282
thaddeas01 <shemthaddeas01@gmail.com>
2022-11-08 15:43:59 +0900

    initialize

---
+++ src/main/java/oit/is/z0553/kaizi/typingskill/TypingskillApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z0553.kaizi.typingskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypingskillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypingskillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z0553/kaizi/typingskill/TypingskillApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z0553.kaizi.typingskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypingskillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
