687063d4be98653248dd89a06013e9aab652bcad
e1b20006 <e1b20006@oit.ac.jp>
2022-11-15 16:45:14 +0900

    date.sqlの実装

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-/*INSERT INTO user (userName) VALUES ('いけだ');
-INSERT INTO user (userName) VALUES ('よしい');
-INSERT INTO user (userName) VALUES ('シェム');
+INSERT INTO users (userName) VALUES ('いけだ');
+INSERT INTO users (userName) VALUES ('よしい');
+INSERT INTO users (userName) VALUES ('シェム');
 INSERT INTO vocabulary (vocab) VALUES ('秘密の部屋');
-INSERT INTO ranking (userName, score) VALUES ('いけだ','100');*/
+INSERT INTO ranking (userName, score) VALUES ('いけだ','100');
